42c3322b9cd52a3010e4f4477c9af557fe221d8e
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-06-05 23:07:53 +0200

    update logic

--- scripts/nomad.sh
+++ scripts/nomad.sh
@@ -13,19 +13,26 @@ which nomad &>/dev/null || {
   popd
 }
 
+IFACE=`route -n | awk '$1 == "192.168.2.0" {print $8}'`
+CIDR=`ip addr show ${IFACE} | awk '$2 ~ "192.168.2" {print $2}'`
+IP=${CIDR%%/24}
+
+grep NOMAD_ADDR ~/.bash_profile || {
+  echo export NOMAD_ADDR=http://${IP}:4646 | tee -a ~/.bash_profile
+}
 
 # check for consul hostname => server
 if [[ "${HOSTNAME}" =~ "nomad-server" ]]; then
 
-  /usr/local/bin/consul members 2>/dev/null || {
-    /usr/local/bin/nomad agent -server  -bind=${IP} -data-dir=/usr/local/nomad -join=192.168.2.21 -join=192.168.2.22 -join=192.168.2.23 -bootstrap-expect=3 >/vagrant/${HOSTNAME}.log &
+  NOMAD_ADDR=http://${IP}:4646 /usr/local/bin/nomad agent-info 2>/dev/null || {
+    /usr/local/bin/nomad agent -server -bind=${IP} -data-dir=/usr/local/nomad -join=192.168.2.21 -join=192.168.2.22 -join=192.168.2.23 -bootstrap-expect=3 >/vagrant/${HOSTNAME}.log &
     sleep 1
   }
 
 else
 
-  /usr/local/bin/consul members 2>/dev/null || {
-    /usr/local/bin/nomad agent  -bind=${IP} -data-dir=/usr/local/nomad -join=192.168.2.21 -join=192.168.2.22 -join=192.168.2.23 -bootstrap-expect=3 >/vagrant/${HOSTNAME}.log &
+  NOMAD_ADDR=http://${IP}:4646 /usr/local/bin/nomad agent-info 2>/dev/null || {
+    /usr/local/bin/nomad agent -client -bind=${IP} -data-dir=/usr/local/nomad -join=192.168.2.21 -join=192.168.2.22 -join=192.168.2.23 >/vagrant/${HOSTNAME}.log &
     sleep 1
   }
 
